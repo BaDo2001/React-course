07c973e7ec23e3c5a3f39771130dc988309fc3fc
Balint Dolla <balint.dolla@gmail.com>
2019-12-28 20:44:18 +0100

    Registers user and sends jwt

--- Contact_Keeper/routes/users.js
+++ Contact_Keeper/routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const config = require('config');
 
 // @route   POST    api/users
 // @desc    Register a user
@@ -35,7 +37,18 @@ router.post('/', [
             user.password = await bcryptjs.hash(password, salt);
             await user.save();
 
-            res.send("User saved");
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn: 3600000, //should be 3600
+            }, (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            });
 
         } catch (error) {
             console.error(error.message);
