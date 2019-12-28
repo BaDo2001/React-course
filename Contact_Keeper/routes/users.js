a966ff1bf0946be4138e79e320216a5e2cbea81e
Balint Dolla <balint.dolla@gmail.com>
2019-12-29 00:37:58 +0100

    Backend done

--- Contact_Keeper/routes/users.js
+++ Contact_Keeper/routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 const config = require('config');
+
+const User = require('../models/User');
+
+const router = express.Router();
 
 // @route   POST    api/users
 // @desc    Register a user
