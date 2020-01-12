ec6ddaac0ce7ee58cdc7aee1f2e8cbaa27907cc1
Balint Dolla <balint.dolla@gmail.com>
2020-01-12 17:01:48 +0100

    Delete contact

--- Contact_Keeper/client/src/context/contact/ContactState.js
+++ Contact_Keeper/client/src/context/contact/ContactState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import uuid from "uuid";
 import ContactContext from "./ContactContext";
-import conatctReducer from "./ContactReducer";
+import contactReducer from "./ContactReducer";
 import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from "../types";
 
 const ContactState = props => {
@@ -31,11 +31,18 @@ const ContactState = props => {
 		]
 	};
 
-	const [state, dispatch] = useReducer(conatctReducer, initialState);
+	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	// Add Contact
+	const addContact = contact => {
+		contact.id = uuid.v4();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 
 	// Delete Contact
+	const deleteContact = id => {
+		dispatch({ type: DELETE_CONTACT, payload: id });
+	};
 
 	// Set Current Contact
 
@@ -50,7 +57,9 @@ const ContactState = props => {
 	return (
 		<ContactContext.Provider
 			value={{
-				contacts: state.contacts
+				contacts: state.contacts,
+				addContact,
+				deleteContact
 			}}
 		>
 			{props.children}
