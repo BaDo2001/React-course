b7fff043f29e9d99b344b5a396da7038ae3e3cc5
BaDo2001 <balint.dolla@gmail.com>
2020-01-21 18:21:17 +0100

    Login + register components

--- Contact_Keeper/client/src/App.js
+++ Contact_Keeper/client/src/App.js
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -20,6 +23,8 @@ const App = () => {
 							<Switch>
 								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
+								<Route exact path="/register" component={Register} />
+								<Route exact path="/login" component={Login} />
 							</Switch>
 						</div>
 					</Fragment>
