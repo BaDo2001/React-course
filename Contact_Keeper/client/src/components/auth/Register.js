67ff20101136b028afe1825bb17c81ba22bde7c8
BaDo2001 <balint.dolla@gmail.com>
2020-01-21 18:50:44 +0100

    Added alertcontext

--- Contact_Keeper/client/src/components/auth/Register.js
+++ Contact_Keeper/client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/AlertContext";
 
 const Register = () => {
+	const alertContext = useContext(AlertContext);
+	const { setAlert } = alertContext;
+
 	const [user, setUser] = useState({
 		name: "",
 		email: "",
@@ -16,7 +20,13 @@ const Register = () => {
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		console.log("Register submit");
+		if (name === "" || email === "" || password === "") {
+			setAlert("Please enter all fields", "danger");
+		} else if (password !== password2) {
+			setAlert("Passowrds do not match", "danger");
+		} else {
+			console.log("Register submit");
+		}
 	};
 
 	return (
@@ -27,15 +37,22 @@ const Register = () => {
 			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label htmlFor="name">Name</label>
-					<input type="text" name="name" value={name} onChange={onChange}></input>
+					<input type="text" name="name" value={name} onChange={onChange} required />
 				</div>
 				<div className="form-group">
 					<label htmlFor="email">Email Address</label>
-					<input type="email" name="email" value={email} onChange={onChange}></input>
+					<input type="email" name="email" value={email} onChange={onChange} required />
 				</div>
 				<div className="form-group">
 					<label htmlFor="password">Password</label>
-					<input type="password" name="password" value={password} onChange={onChange}></input>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={onChange}
+						required
+						minLength="6"
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="password2">Confirm Password</label>
@@ -44,7 +61,9 @@ const Register = () => {
 						name="password2"
 						value={password2}
 						onChange={onChange}
-					></input>
+						required
+						minLength="6"
+					/>
 				</div>
 				<input type="submit" value="Register" className="btn btn-primary btn-block" />
 			</form>
